a0f67988c4ec284dd0d5329d121149d3d99b58f3
sivaraju <sivaraju@gmail.com>
2022-06-17 14:05:19 +0530

    frs-frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    UserLoginComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
